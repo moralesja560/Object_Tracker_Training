d80ad8ef03d28e1bbc2844b461fe38ed781120fc
Jorge_Morales <aalberto_morales@hotmail.com>
2023-11-06 14:36:03 -0600

    image catcher working

---
+++ img_catcher.py
@@ -0,0 +1,199 @@
+import pyads
+import sys
+import threading
+import time
+import sys
+from dotenv import load_dotenv
+import os
+from urllib.request import Request, urlopen
+import json
+from urllib.parse import quote
+import datetime
+import csv
+import pandas as pd
+from datetime import datetime
+import numpy as np
+import cv2
+
+load_dotenv()
+token_Tel = os.getenv('TOK_EN_BOT')
+Jorge_Morales = os.getenv('JORGE_MORALES')
+Paintgroup = os.getenv('PAINTLINE')
+RTSP_URL = 'rtsp://root:mubea@10.65.68.2/axis-media/media.amp'
+os.environ['OPENCV_FFMPEG_CAPTURE_OPTIONS'] = 'rtsp_transport;udp'
+
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    base_path = getattr(sys, '_MEIPASS', os.path.dirname(os.path.abspath(__file__)))
+    return os.path.join(base_path, relative_path)
+
+def My_Documents(location):
+	import ctypes.wintypes
+		#####-----This section discovers My Documents default path --------
+		#### loop the "location" variable to find many paths, including AppData and ProgramFiles
+	CSIDL_PERSONAL = location       # My Documents
+	SHGFP_TYPE_CURRENT = 0   # Get current, not default value
+	buf= ctypes.create_unicode_buffer(ctypes.wintypes.MAX_PATH)
+	ctypes.windll.shell32.SHGetFolderPathW(None, CSIDL_PERSONAL, None, SHGFP_TYPE_CURRENT, buf)
+	#####-------- please use buf.value to store the data in a variable ------- #####
+	#add the text filename at the end of the path
+	temp_docs = buf.value
+	return temp_docs
+
+def send_message(user_id, text,token):
+	global json_respuesta
+	url = f"https://api.telegram.org/{token}/sendMessage?chat_id={user_id}&text={text}"
+	#resp = requests.get(url)
+	#hacemos la petición
+	try:
+		ruta_state = resource_path("images/tele.txt")
+		file_exists = os.path.exists(ruta_state)
+		if file_exists == False:
+			return
+		else:
+			respuesta  = urlopen(Request(url))
+	except Exception as e:
+		print(f"Ha ocurrido un error al enviar el mensaje: {e}")
+	else:
+		#recibimos la información
+		cuerpo_respuesta = respuesta.read()
+		# Procesamos la respuesta json
+		json_respuesta = json.loads(cuerpo_respuesta.decode("utf-8"))
+		print("mensaje enviado exitosamente")
+
+
+
+##--------------------the thread itself--------------#
+
+class hilo1(threading.Thread):
+	#thread init procedure
+	# i think we pass optional variables to use them inside the thread
+	def __init__(self):
+		threading.Thread.__init__(self)
+		self._stop_event = threading.Event()
+	#the actual thread function
+	def run(self):
+
+		#inicialización de PLC
+		while True:
+			try:
+				#sensor
+				plc.open()
+				var_handle46_1 = plc.get_handle('.I_Hang_Counter')
+				cell1 = plc.read_by_name("", plc_datatype=pyads.PLCTYPE_BOOL,handle=var_handle46_1)
+			except Exception as e:
+				#send_message(Jorge_Morales,quote(f"Falla de app: {e}. Si es el 1861, por favor conectarse al PLC via Twincat System Manager. Con eso se hace la conexión ADS"),token_Tel)
+				print(e)
+				plc.release_handle(var_handle46_1)
+				plc.close()
+				break
+			else:
+				if cell1:
+					
+					st = time.time()
+					"""
+					for i in range(0,20):
+						cap = cv2.VideoCapture(RTSP_URL, cv2.CAP_FFMPEG)
+						_, frame = cap.read()
+						#dt = datetime.now()
+						#ts = datetime.timestamp(dt)
+						et = time.time()
+						ts = str(et - st)
+						print(resource_path(f'resources/img{ts}.jpg'))
+						test = cv2.imwrite(resource_path(f'resources/img{ts}.jpg'), frame)
+						print(test)
+					cap.release()
+					"""
+					time.sleep(25.5)
+					cap = cv2.VideoCapture(RTSP_URL, cv2.CAP_FFMPEG)
+					_, frame = cap.read()
+					y=0
+					x=0
+					h=1080
+					w=1350
+					try:
+						crop = frame[y:y+h, x:x+w]
+					except:
+						pass
+					else:
+						et = time.time()
+						ts = str(et - st)
+						print(resource_path(f'resources/img{ts}.jpg'))
+						cv2.imwrite(resource_path(f'resources/img{ts}.jpg'), crop)
+
+
+
+			if self._stop_event.is_set():
+				# close connection
+				print("saliendo")
+				plc.release_handle(var_handle46_1)
+				plc.close()
+				break
+	def stop(self):
+		self._stop_event.set()
+
+	def stopped(self):
+		return self._stop_event.is_set()
+#----------------------end of thread 1------------------#
+
+#---------------Thread 2 Area----------------------#
+class hilo2(threading.Thread):
+	#thread init procedure
+	# i think we pass optional variables to use them inside the thread
+	def __init__(self,thread_name,opt_arg):
+		threading.Thread.__init__(self)
+		self.thread_name = thread_name
+		self.opt_arg = opt_arg
+		self._stop_event = threading.Event()
+	#the actual thread function
+	def run(self):
+		#check for thread1 to keep running
+		while True:
+			if [t for t in threading.enumerate() if isinstance(t, hilo1)]:
+				try:
+					time.sleep(5)
+				except:
+					self._stop_event.set()
+			else:
+				print(f"A problem occurred... Restarting Thread 1")
+				time.sleep(4)
+				thread1 = hilo1()
+				thread1.start()
+				print(f"Thread 1 Started")
+			
+			if self._stop_event.is_set() == True:
+				print("Thread 2 Stopped")
+				break
+
+	def stop(self):
+		self._stop_event.set()
+
+
+
+
+if __name__ == '__main__':
+
+	# connect to the PLC
+	try:
+		pyads.open_port()
+		ams_net_id = pyads.get_local_address().netid
+		print(ams_net_id)
+		pyads.close_port()
+		plc=pyads.Connection('10.65.96.185.1.1', 801, '10.65.96.185')
+	except:
+		print("No se pudo abrir la conexión")
+		sys.exit()
+	 #open the connection
+	else:
+		pass
+	thread1 = hilo1()
+	thread1.start()
+	thread2 = hilo2(thread_name="hilo2",opt_arg="h")
+	thread2.start()
+	while True:
+		stop_signal = input()
+		if stop_signal == "T":
+			thread1.stop()
+			thread2.stop()
+		break
